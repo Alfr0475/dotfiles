67a6b40b9f2d9f659b0bc8c5e0676d819f7eab85
Seiki koga <alfr0475@gmail.com>
2016-10-28 19:16:26 +0900

    インストール済みの文言追加

--- etc/init/osx/20_anyenv.sh
+++ etc/init/osx/20_anyenv.sh
@@ -7,20 +7,23 @@ set -eu
 # common ライブラリ
 . "$DOTPATH"/etc/lib/common.sh
 
-if ! has "anyenv"; then
-    if [ -e "${HOME}/.anyenv" ]; then
-        log_fail "${HOME}/.anyenv: already exists"
-        exit 1
-    fi
-
-    git clone https://github.com/riywo/anyenv "${HOME}"/.anyenv
-
-    log_echo "Install anyenv-update"
-    mkdir -p "${HOME}"/.anyenv/plugins
-    git clone https://github.com/znz/anyenv-update.git "${HOME}"/.anyenv/plugins/anyenv-update
-
-    log_pass "anyenv: installed successfully"
+if has "anyenv"; then
+    log_pass "anyenv: already installed"
+    exit
 fi
+
+if [ -e "${HOME}/.anyenv" ]; then
+    log_fail "${HOME}/.anyenv: already exists"
+    exit 1
+fi
+
+git clone https://github.com/riywo/anyenv "${HOME}"/.anyenv
+
+log_echo "Install anyenv-update"
+mkdir -p "${HOME}"/.anyenv/plugins
+git clone https://github.com/znz/anyenv-update.git "${HOME}"/.anyenv/plugins/anyenv-update
+
+log_pass "anyenv: installed successfully"
 
 # Run the forced termination with a last exit code
 exit $?
